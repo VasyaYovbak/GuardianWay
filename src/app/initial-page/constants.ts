818f7573a13b2e1a2384269aa9a771de731a9640
Vasyl Yovbak <vasyl.yovbak@iongroup.com>
2023-11-28 21:31:56 +0200

    add constants file with predefined POTHOLE_POSITIONS

---
+++ src/app/initial-page/constants.ts
@@ -0,0 +1,39 @@
+import {PotholePosition} from "./models";
+
+export const POTHOLE_POSITIONS: PotholePosition[] = [
+  {
+    x: 530,
+    y: 1245,
+    height: 70,
+    width: 330
+  }, {
+    x: 430,
+    y: 1360,
+    height: 130,
+    width: 600
+  },
+  {
+    x: 825,
+    y: 1120,
+    height: 40,
+    width: 250
+  },
+  {
+    x: 620,
+    y: 1060,
+    height: 100,
+    width: 250
+  },
+  {
+    x: 760,
+    y: 1100,
+    height: 30,
+    width: 120
+  },
+  {
+    x: 660,
+    y: 900,
+    height: 20,
+    width: 160
+  }
+];
