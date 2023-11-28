2b4f2503b52edd3902bb5083aaad86d60e731b0e
Vasyl Yovbak <vasyl.yovbak@iongroup.com>
2023-11-28 21:32:17 +0200

    implement animation and visualization of potholes

--- src/app/initial-page/initial-page.component.ts
+++ src/app/initial-page/initial-page.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {AfterViewInit, Component, inject} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {MatButtonModule} from "@angular/material/button";
-import {RouterLink} from "@angular/router";
+import {Router, RouterLink} from "@angular/router";
+import {InitialPageService} from "./initial-page.service";
+import {PotholePosition} from "./models";
+import {POTHOLE_POSITIONS} from "./constants";
 
 @Component({
   selector: 'app-initial-page',
@@ -10,6 +13,29 @@ import {RouterLink} from "@angular/router";
   templateUrl: './initial-page.component.html',
   styleUrl: './initial-page.component.scss'
 })
-export class InitialPageComponent {
+export class InitialPageComponent implements AfterViewInit {
+  protected isAnimationActive = false;
+  protected renderedPotholePositions: PotholePosition[] = [];
 
+  private _initialPageService = inject(InitialPageService);
+  private _router = inject(Router)
+
+  private _initialPotholes = POTHOLE_POSITIONS;
+
+
+  ngAfterViewInit(): void {
+    requestAnimationFrame(() => {
+      this.renderedPotholePositions = this._initialPotholes
+        .map(pothole => this._initialPageService.getCurrentPotholePosition(pothole))
+    })
+
+  }
+
+  onGoEvent() {
+    this.isAnimationActive = true;
+
+    setTimeout(() => {
+      this._router.navigate(['home'])
+    }, 2000)
+  }
 }
